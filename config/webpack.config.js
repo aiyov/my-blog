1ccf3d1538428873d39baec951bbeb97fd7e1130
litao <1031924189@qq.com>
2018-12-17 17:50:37 +0800

    copy-static-folder

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,82 +6,90 @@ const MinCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const WebpackAssetsManifest = require('webpack-assets-manifest');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        app: './src/index.js',
-    },
-    output: {
-        filename: 'js/[name].bundle.[chunkhash:7].js',
-        path: path.resolve(__dirname, '../dist'),
-        publicPath: './'
-    },
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true // set to true if you want JS source maps
-            }),
-            new OptimizeCSSAssetsPlugin({})
-        ],
-        splitChunks: {
-            chunks: 'all',
-            cacheGroups: {
-                vendors: {
-                    name: 'vender',
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: -10
-                },
-                default: {
-                    minChunks: 2,
-                    priority: -20,
-                    reuseExistingChunk: true
-                }
-            }
+  mode: 'production',
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: 'js/[name].bundle.[chunkhash:7].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: './'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ],
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendors: {
+          name: 'vender',
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
         },
-        runtimeChunk: {
-            name: "manifest"
-        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [MinCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
+    runtimeChunk: {
+      name: "manifest"
     },
-    plugins: [
-        new CleanWebpackPlugin([path.resolve(__dirname, '../dist')], {
-            root: path.resolve(__dirname, '../'),
-            verbose: true
-        }),
-        new HtmlWebpackPlugin({
-            template: './index.html'
-        }),
-        new MinCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        }),
-        new webpack.HashedModuleIdsPlugin(),
-        new WebpackAssetsManifest({
-            publicPath: '/'
-        }),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MinCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
     ]
+  },
+  plugins: [
+    new CleanWebpackPlugin([path.resolve(__dirname, '../dist')], {
+      root: path.resolve(__dirname, '../'),
+      verbose: true
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new MinCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new webpack.HashedModuleIdsPlugin(),
+    new WebpackAssetsManifest({
+      publicPath: '/'
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../static'),
+        to: './',
+        ignore: ['.*']
+      }
+    ])
+  ]
 }
