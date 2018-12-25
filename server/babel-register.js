d6c38aadd4035e49ba1b51c6e788785848924d7c
litao <1031924189@qq.com>
2018-12-25 18:04:02 +0800

    解决babel-register不解析node_modules下文件

--- server/babel-register.js
+++ server/babel-register.js
@@ -1,4 +1,15 @@
 require('@babel/register')({
-    ignore: []
+  ignore: [],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        // module: false,      // <--- typo, not module
+        modules: "commonjs",   // <--- must transpile to commonjs module
+        targets: {"node": process.versions.node},
+        useBuiltIns: "usage"   // <--- not sure if this work
+      }
+    ]
+  ]
 });
 require('./index');
