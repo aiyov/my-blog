24af514a38e3a46a455779571b21987ab5e1fe67
litao <1031924189@qq.com>
2018-12-29 13:12:31 +0800

    webpack server

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -30,7 +30,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        // exclude: /node_modules/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader"
         }
