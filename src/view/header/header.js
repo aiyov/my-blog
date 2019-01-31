221bac66affa67afd278c331e86a8cf3c66c0bae
litao <1031924189@qq.com>
2019-01-31 17:36:10 +0800

    header

--- src/view/header/header.js
+++ src/view/header/header.js
@@ -9,18 +9,37 @@ class HeaderShow extends React.Component {
   constructor(props) {
     super(props)
   }
+
   render() {
     return (
-      <Row type="flex" justify="center">
-        <Col span={20}>
-          <h1 style={{display: 'inline'}}>AIYOV</h1>
-          <Menu mode="horizontal">
-            <Menu.Item key="home">首页</Menu.Item>
-            <Menu.Item key="archived">归档</Menu.Item>
-            <Menu.Item key="about">关于</Menu.Item>
-          </Menu>
-        </Col>
-      </Row>
+      <header id="header">
+        <div id="banner"></div>
+        <div id="header-outer" className="outer">
+          <div id="header-inner" className="inner">
+            <nav id="sub-nav">
+              <a id="nav-search-btn" className="nav-icon" title="搜索"></a>
+            </nav>
+            <div id="search-form-wrap">
+              <form action="//google.com/search" method="get" className="search-form">
+                <input type="search" name="q" className="search-form-input" placeholder="Search"/>
+                <button type="submit" className="search-form-submit">&#xF002;</button>
+                <input type="hidden" name="sitesearch" value="http://findjay.cn"/>
+              </form>
+            </div>
+            <nav id="main-nav">
+              <a id="main-nav-toggle" className="nav-icon"></a>
+              <a className="main-nav-link" href="/">首页</a>
+              <a className="main-nav-link" href="/archives">归档</a>
+              <a className="main-nav-link" href="/about">关于</a>
+            </nav>
+          </div>
+          <div id="header-title" className="inner">
+            <h1 id="logo-wrap">
+              <a href="/" id="logo">AIYOV</a>
+            </h1>
+          </div>
+        </div>
+      </header>
     )
   }
 }
